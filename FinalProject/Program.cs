26d06648326f9553fa2ebad6dd9ed4139038679e
ShahilP2003 <124937285+ShahilP2003@users.noreply.github.com>
2023-03-29 15:38:05 -0600

    This Is The First Commitment
    This is the file that we will work on! This has the hello world Starter Code

---
+++ FinalProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FinalProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
