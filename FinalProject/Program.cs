9159a50e4112ecec09d710d84310adc2b1ac1ffe
Emmanuel Pagaduan <Emmanuel_Pagaduan@outlook.com>
2023-04-18 22:23:36 -0600

    comments on what we need to solve this project

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -2,9 +2,29 @@
 
 namespace FinalProject
 {
+
+    /*
+     probelms:
+        players 
+            - the player must be able to manipulate the table (2d array) 
+                (after choosing one of 7 rows they place a token at the bottom of the collumn)
+            - make the program to switch players after they make a move in the grid
+        Grid
+            - making a 2d array and display it as our game board
+            - (major issue) make a logic to figure if its a connect 4
+                one for vertical, horizontal and diaganol
+     
+     */
     class grid
     {
-        int[,] PlayingGrid = new int[5,5];
+        int[,] PlayingGrid = new int[6,7];
+
+        public int GridDisplay()
+        {
+            int l;
+            l = 1;
+            return l;
+        }
     }
 
 
